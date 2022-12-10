f688cde034a82f92c6b94cd78a1d323740d074d1
henk687 <henkleenheer@upcmail.nl>
2022-12-10 14:16:57 +0100

    changes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -16,17 +16,9 @@ const Card = ({item}) => {
             alt="" 
             className="mainImg"
           />
-          <img 
-            src={
-              process.env.REACT_APP_UPLOAD_URL + item.attributes?.img2?.data?.attributes?.url
-            } 
-            alt="" 
-            className="secondImg"
-          />
         </div>
         <h2>{item?.attributes.title}</h2>
         <div className="prices">
-          <h3>${item.oldPrice || item?.attributes.price + 20}</h3>
           <h3>${item?.attributes.price}</h3>
         </div>
       </div>
